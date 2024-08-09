35c93fba20aabcfe08f069ab826b9d731efb68fe
Kerei1988 <rustam.itchanov@gmail.com>
2024-08-09 09:58:34 +0600

    Доработка кода, согласно ДЗ 0|Домашнее задание по теме "Urls и Views. Функциональное и классовое представление."

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import class_view, func_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', class_view),
+    path('func/', func_view)
 ]
---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def class_view(request):
+    return render(request, "class_view.html")
+
+
+def func_view(request):
+    return render(request, "func_view.html")
